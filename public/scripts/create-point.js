eef2226e7ac5253f30c58703af489be7ac38e2c0
Miquéias <miqueiaswebdesign@gmail.com>
2020-06-05 16:29:46 -0300

    ecoleta

---
+++ public/scripts/create-point.js
@@ -0,0 +1,95 @@
+function populateUFs(){
+    const ufSelect = document.querySelector("select[name=uf]");
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( (resp) => {
+        return resp.json();
+    })
+    .then( (states) => {
+        
+        for(const state of states){
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`; 
+        }
+        
+        
+    })
+}
+
+populateUFs();
+
+function getCities(event){
+    const citySelect = document.querySelector("select[name=city]");
+    const stateInput = document.querySelector("input[name=state]");
+
+    const ufValue = event.target.value;
+
+    const indexOfSelectedState = event.target.selectedIndex;
+    
+    stateInput.value = event.target.options[indexOfSelectedState].text;
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    citySelect.innerHTML = "<option value>Selecione a cidade...</option>";
+    citySelect.disabled = true; 
+
+    fetch(url)
+    .then( (resp) => {
+        return resp.json();
+    })
+    .then( (cities) => {
+
+        for(const city of cities){
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`; 
+        }
+        
+        citySelect.disabled = false;   
+    });
+}
+
+
+document.querySelector("select[name=uf]")
+.addEventListener("change", getCities);
+
+
+
+//Itens de coleta
+const itensToCollect = document.querySelectorAll('.itens-grid li');
+
+for(const item of itensToCollect){
+    item.addEventListener('click', handleSelectedItem);
+}
+
+
+const collectedItens = document.querySelector("input[name=items]");
+let selectItens = [];
+
+
+function handleSelectedItem(event){
+    const itemLi = event.target;
+
+    itemLi.classList.toggle("selected");
+
+    const itemId = itemLi.dataset.id; 
+
+
+    const alreadySelected  = selectItens.findIndex(function(item){
+        const itemFound = item == itemId;
+
+        return itemFound;
+    })
+
+
+    if(alreadySelected >= 0){
+        const filteredItens = selectItens.filter(function(item){
+            const itemIsDifferent = item != itemId;
+            return itemIsDifferent;
+
+        });
+        selectItens = filteredItens;  
+    }else{
+        selectItens.push(itemId);
+    }
+
+    collectedItens.value = selectItens;
+  
+
+}
